ab89ff37dcc5fb205749c5ad91992b9843cf9a8d
Eytan Abramowitz <eytan.abramowitz@gmail.com>
2026-06-11 10:18:37 -0400

    Defined function to load tasks from json file

--- todo.py
+++ todo.py
@@ -1,1 +1,14 @@
+import os
+import json
+
+TASK_FILE = "task.json"
+
+def load_tasks():
+    if not os.path.exists.(TASK_FILE)
+        with open(TASK_FILE) as f
+            json.dump([], f)
+        return []
+    with open(TASK_FILE, "r") as f:
+        return json.load(f)
+
 print("Todo CLI")
