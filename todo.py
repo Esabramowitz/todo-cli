b11840dd9da7d256db1f6bbdb80012fdabf60f70
Eytan Abramowitz <eytan.abramowitz@gmail.com>
2026-06-11 10:35:21 -0400

    Recomitted last comitted. Didn't commit properly

--- todo.py
+++ todo.py
@@ -1,14 +1,30 @@
 import os
 import json
 
-TASK_FILE = "task.json"
+TASK_FILE = "tasks.json"
 
 def load_tasks():
-    if not os.path.exists.(TASK_FILE)
-        with open(TASK_FILE) as f
+    if not os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "w") as f:
             json.dump([], f)
         return []
     with open(TASK_FILE, "r") as f:
         return json.load(f)
+
+def add_task(tasks, text):
+    new_id = len(tasks) + 1
+    new_task = {
+        "id": new_id,
+        "text": text,
+        "done": False
+    }
+    tasks.append(new_task)
+    return tasks
+
+
+def save_tasks(tasks):
+    with open(TASK_FILE, "w") as f:
+        json.dump(tasks, f, indent=2)
+
 
 print("Todo CLI")
